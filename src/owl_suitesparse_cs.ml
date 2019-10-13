c705dbef01072a68a0a689ca061827e3a5b2e095
Liang Wang <liang.wang@nokia.com>
2019-10-13 10:50:14 +0300

    coding owl-suitesparse ...

--- src/owl_suitesparse_cs.ml
+++ src/owl_suitesparse_cs.ml
@@ -1,7 +1,5 @@
 
 
-
-
 let cs_dl_spfree x =
   Owl_suitesparse_ffi.CS.cs_dl_spfree x
   |> ignore
@@ -45,7 +43,40 @@ let cs_dl_add ~x ~y ~alpha ~beta =
   z
 
 
-let cs_dl_dupl ~x = 
+let cs_dl_multiply ~x ~y =
+  let z = Owl_suitesparse_ffi.CS.cs_dl_multiply x y in
+  Gc.finalise cs_dl_spfree z;
+  z
+
+
+let cs_dl_dupl x = 
   let result = Owl_suitesparse_ffi.CS.cs_dl_dupl x in
   let result = Int64.to_int result in
   Owl_suitesparse_exception.fail_on_nonzero result "cs_dl_dupl"
+
+
+let cs_dl_transpose ~x ~values =
+  let y = Owl_suitesparse_ffi.CS.cs_dl_transpose x values in
+  Gc.finalise cs_dl_spfree y;
+  y
+
+
+let cs_dl_norm x =
+  let result = Owl_suitesparse_ffi.CS.cs_dl_norm x in
+  result
+
+
+let cs_dl_lusol ~order ~x ~tol =
+  let b_ptr = Ctypes.(allocate double 0.) in
+  let result = Owl_suitesparse_ffi.CS.cs_dl_lusol order x b_ptr tol in
+  let result = Int64.to_int(result) in
+  Owl_suitesparse_exception.fail_on_nonzero result "cs_dl_lusol";
+  Ctypes.(!@b_ptr)
+
+
+let cs_dl_qrsol ~order ~x =
+  let b_ptr = Ctypes.(allocate double 0.) in
+  let result = Owl_suitesparse_ffi.CS.cs_dl_qrsol order x b_ptr in
+  let result = Int64.to_int(result) in
+  Owl_suitesparse_exception.fail_on_nonzero result "cs_dl_qrsol";
+  Ctypes.(!@b_ptr)

ca946c00323c0c9ef5f6bdb7317c53a5308a2877
Liang Wang <liang.wang@nokia.com>
2019-10-13 23:15:14 +0300

    coding owl-suitesparse ...

--- bindings/ffi_suitesparse_bindings_umfpack_d.ml
+++ bindings/ffi_suitesparse_bindings_umfpack_d.ml
@@ -20,4 +20,8 @@ module Make (F : Cstubs.FOREIGN) = struct
 
   let umfpack_dl_report_symbolic = foreign "umfpack_dl_report_symbolic" (ptr void @-> ptr double @-> returning int64_t)
 
+  let umfpack_dl_solve = foreign "umfpack_dl_solve" (int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr double @-> ptr double @-> ptr double @-> ptr void @-> ptr double @-> ptr double @-> returning int64_t)
+
+  let umfpack_dl_free_symbolic = foreign "umfpack_dl_free_symbolic" (ptr (ptr void) @-> returning void)
+
 end
--- bindings/ffi_suitesparse_bindings_umfpack_z.ml
+++ bindings/ffi_suitesparse_bindings_umfpack_z.ml
@@ -20,4 +20,8 @@ module Make (F : Cstubs.FOREIGN) = struct
 
   let umfpack_zl_report_symbolic = foreign "umfpack_zl_report_symbolic" (ptr void @-> ptr double @-> returning int64_t)
 
+  let umfpack_zl_solve = foreign "umfpack_zl_solve" (int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr double @-> ptr double @-> ptr double @-> ptr double @-> ptr double @-> ptr double @-> ptr void @-> ptr double @-> ptr double @-> returning int64_t)
+
+  let umfpack_zl_free_symbolic = foreign "umfpack_zl_free_symbolic" (ptr (ptr void) @-> returning void)
+
 end

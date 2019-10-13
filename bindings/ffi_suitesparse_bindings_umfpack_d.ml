e98f2063f5b29cfadee6954ea586b56dfa1298c7
Liang Wang <liang.wang@nokia.com>
2019-10-13 20:43:39 +0300

    coding owl-suitesparse ...

--- bindings/ffi_suitesparse_bindings_umfpack_d.ml
+++ bindings/ffi_suitesparse_bindings_umfpack_d.ml
@@ -16,6 +16,8 @@ module Make (F : Cstubs.FOREIGN) = struct
 
   (** core functions - double *)
 
-  let umfpack_dl_numeric = foreign "umfpack_dl_numeric" (ptr int64_t @-> ptr int64_t @-> ptr double @-> ptr void @-> ptr (ptr void) @-> ptr double @-> ptr double @-> returning int)
+  let umfpack_dl_numeric = foreign "umfpack_dl_numeric" (ptr int64_t @-> ptr int64_t @-> ptr double @-> ptr void @-> ptr (ptr void) @-> ptr double @-> ptr double @-> returning int64_t)
+
+  let umfpack_dl_report_symbolic = foreign "umfpack_dl_report_symbolic" (ptr void @-> ptr double @-> returning int64_t)
 
 end
--- bindings/ffi_suitesparse_bindings_umfpack_z.ml
+++ bindings/ffi_suitesparse_bindings_umfpack_z.ml
@@ -16,6 +16,8 @@ module Make (F : Cstubs.FOREIGN) = struct
 
   (** core functions - complex *)
 
-  let umfpack_zl_numeric = foreign "umfpack_zl_numeric" (ptr int64_t @-> ptr int64_t @-> ptr double @-> ptr double @-> ptr void @-> ptr (ptr void) @-> ptr double @-> ptr double @-> returning int)
+  let umfpack_zl_numeric = foreign "umfpack_zl_numeric" (ptr int64_t @-> ptr int64_t @-> ptr double @-> ptr double @-> ptr void @-> ptr (ptr void) @-> ptr double @-> ptr double @-> returning int64_t)
+
+  let umfpack_zl_report_symbolic = foreign "umfpack_zl_report_symbolic" (ptr void @-> ptr double @-> returning int64_t)
 
 end

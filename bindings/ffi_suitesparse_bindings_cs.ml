031fc875fb9ea9a68970f577e136d1191dc7858c
Liang Wang <liang.wang@nokia.com>
2019-10-13 13:04:57 +0300

    coding owl-suitesparse ...

--- bindings/ffi_suitesparse_bindings_cs.ml
+++ bindings/ffi_suitesparse_bindings_cs.ml
@@ -21,7 +21,7 @@ module Make (F : Cstubs.FOREIGN) = struct
     let cs_dld : cs_dl structure typ = structure "cs_dl_dmperm_results"
   
 
-    (** core functions *)
+    (** core functions - double *)
 
     let cs_dl_add = foreign "cs_dl_add" (ptr cs_dl @-> ptr cs_dl @-> double @-> double @-> returning (ptr cs_dl))
 
@@ -47,7 +47,7 @@ module Make (F : Cstubs.FOREIGN) = struct
 
     let cs_dl_print = foreign "cs_dl_print" (ptr cs_dl @-> int64_t @-> returning int64_t)
 
-    (* let cs_dl_load = foreign "cs_dl_load" (ptr file @-> returning (ptr cs_dl)) *)
+    (* let cs_dl_load = foreign "cs_dl_load" (ptr FILE @-> returning (ptr cs_dl)) *)
 
     let cs_dl_amd = foreign "cs_dl_amd" (int64_t @-> ptr cs_dl @-> returning (ptr int64_t))
 
@@ -89,11 +89,59 @@ module Make (F : Cstubs.FOREIGN) = struct
 
     let cs_dl_updown = foreign "cs_dl_updown" (ptr cs_dl @-> int64_t @-> ptr cs_dl @-> ptr int64_t @-> returning int64_t)
 
+    let cs_dl_counts = foreign "cs_dl_counts" (ptr cs_dl @-> ptr int64_t @-> ptr int64_t @-> int64_t @-> returning (ptr int64_t))
+
+    let cs_dl_cumsum = foreign "cs_dl_cumsum" (ptr int64_t @-> ptr int64_t @-> int64_t @-> returning double)
+
+    let cs_dl_dfs = foreign "cs_dl_dfs" (int64_t @-> ptr cs_dl @-> int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr int64_t @-> returning int64_t)
+
+    let cs_dl_etree = foreign "cs_dl_etree" (ptr cs_dl @-> int64_t @-> returning (ptr int64_t))
+
+    (* let cs_dl_fkeep = foreign "cs_dl_fkeep" (ptr cs_dl @-> ptr int64_t @-> ptr int64_t @-> int64_t @-> returning int64_t) *)
+
+    let cs_dl_house = foreign "cs_dl_house" (ptr double @-> ptr double @-> int64_t @-> returning double)
+
+    let cs_dl_maxtrans = foreign "cs_dl_maxtrans" (ptr cs_dl @-> int64_t @-> returning (ptr int64_t))
+
+    let cs_dl_post = foreign "cs_dl_post" (ptr int64_t @-> int64_t @-> returning (ptr int64_t))
+
+    let cs_dl_scc = foreign "cs_dl_scc" (ptr cs_dl @-> returning (ptr cs_dld))
+
+    let cs_dl_scatter = foreign "cs_dl_scatter" (ptr cs_dl @-> int64_t @-> double @-> ptr int64_t @-> ptr double @-> int64_t @-> ptr cs_dl @-> int64_t @-> returning int64_t)
+
+    let cs_dl_tdfs = foreign "cs_dl_tdfs" (int64_t @-> int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr int64_t @-> returning int64_t)
+
+    let cs_dl_leaf = foreign "cs_dl_leaf" (int64_t @-> int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr int64_t @-> returning int64_t)
+
+    let cs_dl_reach = foreign "cs_dl_reach" (ptr cs_dl @-> ptr cs_dl @-> int64_t @-> ptr int64_t @-> ptr int64_t @-> returning int64_t)
+
+    let cs_dl_spsolve = foreign "cs_dl_spsolve" (ptr cs_dl @-> ptr cs_dl @-> int64_t @-> ptr int64_t @-> ptr double @-> ptr int64_t @-> int64_t @-> returning int64_t)
+
+    let cs_dl_ereach = foreign "cs_dl_ereach" (ptr cs_dl @-> int64_t @-> ptr int64_t @-> ptr int64_t @-> ptr int64_t @-> returning int64_t)
+
+    let cs_dl_randperm = foreign "cs_dl_randperm" (int64_t @-> int64_t @-> returning (ptr int64_t))
+
 
     (** utility functions *)
 
     let cs_dl_spalloc = foreign "cs_dl_spalloc" (int64_t @-> int64_t @-> int64_t @-> int64_t @-> int64_t @-> returning (ptr cs_dl))
 
     let cs_dl_spfree = foreign "cs_dl_spfree" (ptr cs_dl @-> returning (ptr cs_dl))
+
+    let cs_dl_sfree = foreign "cs_dl_sfree" (ptr cs_dls @-> returning (ptr cs_dls))
+
+    let cs_dl_nfree = foreign "cs_dl_nfree" (ptr cs_dln @-> returning (ptr cs_dln))
+
+    let cs_dl_dfree = foreign "cs_dl_dfree" (ptr cs_dld @-> returning (ptr cs_dld))
+
+    let cs_dl_dalloc = foreign "cs_dl_dalloc" (int64_t @-> int64_t @-> returning (ptr cs_dld))
+
+    let cs_dl_done = foreign "cs_dl_done" (ptr cs_dl @-> ptr void @-> ptr void @-> int64_t @-> returning (ptr cs_dl))
+
+    let cs_dl_idone = foreign "cs_dl_idone" (ptr int64_t @-> ptr cs_dl @-> ptr void @-> int64_t @-> returning (ptr int64_t))
+
+    let cs_dl_ndone = foreign "cs_dl_ndone" (ptr cs_dln @-> ptr cs_dl @-> ptr void @-> ptr void @-> int64_t @-> returning (ptr cs_dln))
+
+    let cs_dl_ddone = foreign "cs_dl_ddone" (ptr cs_dld @-> ptr cs_dl @-> ptr void @-> int64_t @-> returning (ptr cs_dld))
 
 end

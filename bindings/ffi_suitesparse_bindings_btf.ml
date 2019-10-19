7902e9020aff6a4f54b15d0ae5a722e5fd97f8a3
Liang Wang <liang.wang@nokia.com>
2019-10-19 16:20:20 +0300

    coding owl-suitesparse ...

--- bindings/ffi_suitesparse_bindings_btf.ml
+++ bindings/ffi_suitesparse_bindings_btf.ml
@@ -15,59 +15,24 @@ module Make (F : Cstubs.FOREIGN) = struct
 
   (** core functions *)
 
-  let ccolamd_l_recommended =
-    foreign "ccolamd_l_recommended" (int64_t @-> int64_t @-> int64_t @-> returning size_t)
-
-
-  let ccolamd_l_set_defaults =
-    foreign "ccolamd_l_set_defaults" (ptr double @-> returning void)
-
-
-  let ccolamd_l =
+  let btf_l_maxtrans =
     foreign
-      "ccolamd_l"
+      "btf_l_maxtrans"
       (int64_t
       @-> int64_t
-      @-> int64_t
       @-> ptr int64_t
       @-> ptr int64_t
+      @-> double
       @-> ptr double
       @-> ptr int64_t
       @-> ptr int64_t
       @-> returning int64_t)
 
 
-  let csymamd_l =
+  let btf_l_strongcomp =
     foreign
-      "csymamd_l"
+      "btf_l_strongcomp"
       (int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr double
-      @-> ptr int64_t
-      @-> ptr void
-      @-> ptr void
-      @-> ptr int64_t
-      @-> int64_t
-      @-> returning int64_t)
-
-
-  let ccolamd_l_report = foreign "ccolamd_l_report" (ptr int64_t @-> returning void)
-  let csymamd_l_report = foreign "csymamd_l_report" (ptr int64_t @-> returning void)
-
-  let ccolamd2_l =
-    foreign
-      "ccolamd2_l"
-      (int64_t
-      @-> int64_t
-      @-> int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr double
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
       @-> ptr int64_t
       @-> ptr int64_t
       @-> ptr int64_t
@@ -77,50 +42,15 @@ module Make (F : Cstubs.FOREIGN) = struct
       @-> returning int64_t)
 
 
-  let ccolamd_l_apply_order =
+  let btf_l_order =
     foreign
-      "ccolamd_l_apply_order"
-      (ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> int64_t
-      @-> int64_t
-      @-> returning void)
-
-
-  let ccolamd_l_fsize =
-    foreign
-      "ccolamd_l_fsize"
+      "btf_l_order"
       (int64_t
       @-> ptr int64_t
       @-> ptr int64_t
+      @-> double
+      @-> ptr double
       @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> returning void)
-
-
-  let ccolamd_l_postorder =
-    foreign
-      "ccolamd_l_postorder"
-      (int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> ptr int64_t
-      @-> returning void)
-
-
-  let ccolamd_l_post_tree =
-    foreign
-      "ccolamd_l_post_tree"
-      (int64_t
-      @-> int64_t
       @-> ptr int64_t
       @-> ptr int64_t
       @-> ptr int64_t

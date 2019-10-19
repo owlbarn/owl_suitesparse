0ab9b57fb943fccea3478bc2e2a3511a5dff76f9
Liang Wang <liang.wang@nokia.com>
2019-10-19 19:02:16 +0300

    coding owl-suitesparse ...

--- bindings/ffi_suitesparse_bindings_klu.ml
+++ bindings/ffi_suitesparse_bindings_klu.ml
@@ -161,4 +161,195 @@ module Make (F : Cstubs.FOREIGN) = struct
     foreign
       "klu_zl_free_numeric"
       (ptr (ptr klu_l_numeric) @-> ptr klu_l_common @-> returning int64_t)
+
+
+  let klu_l_sort =
+    foreign
+      "klu_l_sort"
+      (ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_zl_sort =
+    foreign
+      "klu_zl_sort"
+      (ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_l_flops =
+    foreign
+      "klu_l_flops"
+      (ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_zl_flops =
+    foreign
+      "klu_zl_flops"
+      (ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_l_rgrowth =
+    foreign
+      "klu_l_rgrowth"
+      (ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_zl_rgrowth =
+    foreign
+      "klu_zl_rgrowth"
+      (ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_l_condest =
+    foreign
+      "klu_l_condest"
+      (ptr int64_t
+      @-> ptr double
+      @-> ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_zl_condest =
+    foreign
+      "klu_zl_condest"
+      (ptr int64_t
+      @-> ptr double
+      @-> ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_l_rcond =
+    foreign
+      "klu_l_rcond"
+      (ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_zl_rcond =
+    foreign
+      "klu_zl_rcond"
+      (ptr klu_l_symbolic
+      @-> ptr klu_l_numeric
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_l_scale =
+    foreign
+      "klu_l_scale"
+      (int64_t
+      @-> int64_t
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_zl_scale =
+    foreign
+      "klu_zl_scale"
+      (int64_t
+      @-> int64_t
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_l_extract =
+    foreign
+      "klu_l_extract"
+      (ptr klu_l_numeric
+      @-> ptr klu_l_symbolic
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_zl_extract =
+    foreign
+      "klu_zl_extract"
+      (ptr klu_l_numeric
+      @-> ptr klu_l_symbolic
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr int64_t
+      @-> ptr double
+      @-> ptr int64_t
+      @-> ptr klu_l_common
+      @-> returning int64_t)
+
+
+  let klu_l_malloc =
+    foreign "klu_l_malloc" (size_t @-> size_t @-> ptr klu_l_common @-> returning void)
+
+
+  let klu_l_free =
+    foreign
+      "klu_l_free"
+      (ptr void @-> size_t @-> size_t @-> ptr klu_l_common @-> returning void)
+
+
+  let klu_l_realloc =
+    foreign
+      "klu_l_realloc"
+      (size_t @-> size_t @-> size_t @-> ptr void @-> ptr klu_l_common @-> returning void)
 end
